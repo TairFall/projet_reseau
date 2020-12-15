d37da99c5dff015a23b7e6097ee430eed3a992b8
GreaterDogFR <benjaminherve76120@outlook.fr>
2020-12-15 16:47:09 +0100

    récupération de la fonction hex

---
+++ asset/js/benjamin.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $.ajax()
+});
+//Récupérer le fichier json avec ajax
+// 2. Récup donnée
+// 3. formatage/filtrage/Traitement donnée récup
+// 4. ajout script/fichier php qui gère la bdd, tjrs en ajax;
+//$.ajax()
+//5. donneés envoyés au script php
+//  check des données
+//  insertion en bdd
