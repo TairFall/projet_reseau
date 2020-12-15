f15d39bb66627e06174764e0240e017033d0129d
VitorinoNzo <vitorinonzo@gmail.com>
2020-12-15 12:05:50 +0100

    header slider

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,1 +1,15 @@
-console.log('test ok');
+// console.log('test ok');
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      animationLoop: true,
+      animationSpeed: 1500,
+      controlNav: false,
+      directionNav:false,
+      
+    });
+
+
+
+});
