ac50a7b5d971e39f39bfa8f28c2261549d87dd56
TairFall <tairfall76@sfr.fr>
2020-12-15 16:42:26 +0100

    About us bientôt terminé

--- aboutus.php
+++ aboutus.php
@@ -23,7 +23,7 @@ require('inc/functions.php');
   <link href="template/css/landing-page.min.css" rel="stylesheet">
 
   <!-- Perso css -->
-  <link rel="stylesheet" href="asset/css/style.css">
+  <link rel="stylesheet" href="asset/css/tair.css">
 </head>
 
 <body>
@@ -38,70 +38,101 @@ require('inc/functions.php');
 
   <!-- Masthead -->
   <header class="masthead text-white text-center">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <h1 class="display-4">Nous sommes NetworkVIP.</h1>
+    <h1 class="display-4">Nous prenons nos responsabilités.</h1>
+    <p class="lead">En tant que spécialiste de l'analyse réseau, notre entreprise s'attèle, aux cotés de ses clients, à .......................</p>
   </header>
 
-  <div class="container border border-dark">
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Fluid jumbotron</h1>
-        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-      </div>
-    </div>
-    <div class="card-deck">
-      <div class="card .bg-light text-primary">
-        <img class="card-img-top" src="asset/img/test.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-      </div>
-      <div class="card .bg-light text-primary">
-        <img class="card-img-top" src="asset/img/security.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-      </div>
-      <div class="card .bg-light text-primary">
-        <img class="card-img-top" src="asset/img/people.png" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-      </div>
-    </div>
-    <hr>
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div id="firstslide"class="carousel-inner">
-        <div class="carousel-item active">
-          <div class="textcarou1">
-            <h1>LOREM IPSUM</h1>
+  <div class="container">
+    <div class="contenant bg-light">
+      <div class="card-deck">
+        <div class="card .bg-light text-primary">
+          <img class="card-img-top" src="asset/img/test.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Notre mode opératoire</h5>
+            <p class="card-text">Nous apprécions les difficultés liées à la complexité des infrastructures informatiques et réseaux, nos produits donnent à nos clients les moyens de superviser tout, et les aident ainsi à optimiser leurs ressources.</p>
           </div>
-          <img src="https://www.faveohelpdesk.com/wp-content/uploads/2015/06/1.jpg" class="d-block w-100" alt="...">
         </div>
-        <div class="carousel-item">
-          <img src="https://www.icademie.com/img/photos/code-informatique-60-1920x1080.jpg" class="d-block w-100" alt="...">
+        <div class="card .bg-light text-primary">
+          <img class="card-img-top" src="asset/img/security.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Notre motivation</h5>
+            <p class="card-text">Nous pensons que la sécurité des informations de nos clients est d'une importance cruciale, nous mettons donc tout en oeuvres pour la garantir.</p>
+          </div>
         </div>
-        <div class="carousel-item">
-          <img src="https://micromijo.com/img/assistance-informatique.jpg" class="d-block w-100" alt="...">
+        <div class="card .bg-light text-primary">
+          <img class="card-img-top" src="asset/img/people.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Notre domaine de compétence</h5>
+            <p class="card-text">Nous proposons des solutions de supervision aux particuliers dans le but de simplifier leurs tâches, en collaboration avec des partenaires de renom pour surmonter les défis de la supervision dans un monde virtuel en perpétuelle évolution.</p>
+          </div>
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-  </div>
-  <?php
-  include('inc/footer.php');
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div id="firstslide" class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://www.faveohelpdesk.com/wp-content/uploads/2015/06/1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="https://liveassets.ca/wp-content/uploads/2018/09/123456.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="https://www.bruce.work/images/job/D1408_006_16_9_1920_1x.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <div class="valeurs_group">
+        <h2>Nos valeurs</h2>
+        <div class="card-deck">
+          <div class="card .bg-light text-primary">
+            <div class="card-body">
+              <h5 class="card-title">Nous plaçons le client
+                au centre</h5>
+                <p class="card-text">
+                  Nous sommes nous-mêmes des professionnels de l’informatique. Le client est au cœur de tout ce que nous entreprenons.</p>
+                </div>
+              </div>
+              <div class="card .bg-light text-primary">
+                <div class="card-body">
+                  <h5 class="card-title">Nous cultivons
+                    l’esprit d’équipe</h5>
+                    <p class="card-text">Nous sommes heureux de transmettre nos connaissances entre collègues et toujours prêts à accueillir de nouveaux membres dans notre équipe.</p>
+                  </div>
+                </div>
+                <div class="card .bg-light text-primary">
+                  <div class="card-body">
+                    <h5 class="card-title">Nous visons
+                      l’excellence</h5>
+                      <p class="card-text">Nous remettons systématiquement en question nos pratiques et solutions pour faire rimer PRTG avec qualité.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="team">
+                <h2>Notre équipe</h2>
+                <img class="rounded-circle" height="180" src="https://hlassets.paessler.com/common/files/portraits/square-grey/hbinder.jpg" />
+                <p>Benjamin</p>
+                <img class="rounded-circle" height="180" src="https://hlassets.paessler.com/common/files/portraits/square-grey/hbinder.jpg" />
+                <p>Benjamin</p>
+                <img class="rounded-circle" height="180" src="https://hlassets.paessler.com/common/files/portraits/square-grey/hbinder.jpg" />
+                <p>Benjamin</p>
+                <img class="rounded-circle" height="180" src="https://hlassets.paessler.com/common/files/portraits/square-grey/hbinder.jpg" />
+                <p>Benjamin</p>
+              </div>
+            </div>
+          </div>
+          <?php
+          include('inc/footer.php');
