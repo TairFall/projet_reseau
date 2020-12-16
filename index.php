001f45eb3eefe76bb5342985718f4cb082c0a76c
VitorinoNzo <vitorinonzo@gmail.com>
2020-12-16 17:03:57 +0100

    header homepage

--- index.php
+++ index.php
@@ -3,7 +3,37 @@
 include('inc/header.php');
 require('inc/functions.php');
 ?>
-
+<div class="container">
+    <div class="row">
+        <div class="col align-self-start">
+            <div class="card mb-1">
+                <img class="card-img-top" src="asset/flexslider/img/reseau.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Sécurisé :</h5>
+                    <p class="card-text">Aucunes données personelles n'est stockés</p>
+                </div>
+            </div>
+        </div>
+        <div class="col align-self-end">
+            <div class="card mb-2">
+                <img class="card-img-top" src="asset/flexslider/img/fibre.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col align-self-start">
+            <div class="card mb-3">
+                <img class="card-img-top" src="asset/flexslider/img/fibre2.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Simple et rapide :</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php
