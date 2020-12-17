2321f6a8031b2e4aabd92f1f383029bca674a07d
GreaterDogFR <benjaminherve76120@outlook.fr>
2020-12-17 16:45:44 +0100

    footer ok

--- inc/footer.php
+++ inc/footer.php
@@ -1,29 +1,32 @@
 
 
 
-  <!-- Footer -->
-  <footer>
+    <!-- Footer -->
+    <footer>
     <div class="container-fluid">
     <!-- section contact -->
-      <div class="d-flex justify-content-around" id="containerfooter">
-        <a class="text" href="#">Nous contacter</a>
+      <div class="d-flex justify-content-around mt-5 mb-5 ml-4 mr-4 text-white" id="containerfooter">
+        <a class="text-decoration-none" href="#">Nous contacter</a>
         &#124
-        <a href="#">F.A.Q</a>
+        <a class="text-decoration-none" href="#">F.A.Q</a>
         &#124
-        <a href="#">Mentions légales</a>
+        <a class="text-decoration-none" href="#">Mentions légales</a>
       </div>
+
       <!-- section reseau sociaux -->
-      <div> 
-        <ul id="social-icons" >
-          <li><a href="#" class="facebook"></a></li>
-          <li><a href="#" class="twitter"></a></li>
-          <li><a href="#" class="snapchat"></a></li>
-          <li><a href="#" class="instagram"></a></li>
-        </ul>
+      <div class="mt-5 mb-5"></div>
+
+      <div class="d-flex justify-content-around mt-5 mb-5" id="social-icons"> 
+          <a href="#" class="facebook"></a>
+          <a href="#" class="twitter"></a>
+          <a href="#" class="snapchat"></a>
+          <a href="#" class="instagram"></a>
       </div>
+
+      <div class="mt-5 mb-5"></div>
       <!-- section copyright -->
-      <div class="container-fluid d-flex justify-content-around" id="copyright">
-          <p>VIP Network est édité par VIP Network Team. Copyright &copy VIP Network Team 2020-2021. Tous droits réservés </p>
+      <div class="container-fluid d-flex justify-content-center mt-5 mb-5" id="copyright">
+          <p class="text-white">VIP Network est édité par VIP Network Team. Copyright &copy VIP Network Team 2020-2021. Tous droits réservés </p>
       </div>
     </div>
   </footer>
