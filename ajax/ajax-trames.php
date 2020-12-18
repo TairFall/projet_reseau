07e563dd4defe254d7febc0c6da8894e2e743418
unknown <>
2020-12-18 12:14:09 +0100

    ip hexa to ip ok

--- ajax/ajax-trames.php
+++ ajax/ajax-trames.php
@@ -5,14 +5,35 @@ require('../inc/pdo.php');
 //print_r($_POST['trames']);
 
 $trames = $_POST['trames'];
-
+$tableau_conv = [
+  //  CHIFFRES
+  "0" => [0],
+  "1" => [1],
+  "2" => [2],
+  "3" => [3],
+  "4" => [4],
+  "5" => [5],
+  "6" => [7],
+  "8" => [8],
+  "9" => [9],
+  //  MINUSCULES
+  "a" => [10],
+  "b" => [11],
+  "c" => [12],
+  "d" => [13],
+  "e" => [14],
+  "f" => [15],
+  // MAJUSCULES
+  "A" => [10],
+  "B" => [11],
+  "C" => [12],
+  "D" => [13],
+  "E" => [14],
+  "F" => [15]
+];
 foreach ($trames as $trame) {
 
-  if(isset($trame['protocol']['type']))
-  {
-    $protocol_type = $trame['protocol']['type'];
-    $protocol_code = $trame['protocol']['code'];
-  }
+
 
   $datet = $trame['date'];
   $version = $trame['version'];
@@ -23,36 +44,35 @@ foreach ($trames as $trame) {
   $ttl = $trame['ttl'];
   $protocol_name = $trame['protocol']['name'];
   $protocol_flags_code = "";
+  // LE PROCOCOLE UPD N'A PAS DE FLAGS CODE
   if (isset($trame['protocol']['flags'])){
     $protocol_flags_code = $trame['protocol']['flags']['code'];
+  }
+  $protocol_type = "";
+  $protocol_code = "";
+  if(isset($trame['protocol']['type']))
+  {
+    $protocol_type = $trame['protocol']['type'];
+    $protocol_code = $trame['protocol']['code'];
   }
   $protocol_checksum_status = $trame['protocol']['checksum']['status'];
   $protocol_ports_from = $trame['protocol']['ports']['from'];
   $protocol_ports_dest = $trame['protocol']['ports']['dest'];
   $headerChecksum = $trame['headerChecksum'];
+  /// IP HEXA TO IP COOL
+
   $ip_from = $trame['ip']['from'];
+  $broke_ip = splite($ip_from);
+  $new_ip = hexatoIp($tableau_conv,$broke_ip);
+
+  //// END OF HEAX TO IP /////
+
   $ip_dest = $trame['ip']['dest'];
 
   // INSERTION
 
-  if(isset($trame['protocol']['type']))
-  {
-    /// VERIF QUE TOTO N'EXISTE PAS DEJA POUR PAS FAIRE DOUBLON BDD
-    $sql = "SELECT * FROM trames WHERE datet = '$datet' AND protocol_name = '$protocol_name' AND identification = '$identification' AND protocol_flags_code = '$protocol_flags_code'";
-    $query = $pdo->prepare($sql);
-    $query->execute();
-    $trameExist = $query->fetch();
 
-
-    if(empty($trameExist)){
-      $sql = "INSERT INTO trames (datet,version,headerLength,service,identification,flags_code,ttl,protocol_name,protocol_flags_code,protocol_checksum_status,protocol_ports_from,protocol_ports_dest,protocol_type,protocol_code,headerChecksum,ip_from,ip_dest) VALUES (".$datet.",".$version.",".$headerLength.",'".$service."','".$identification."','".$flags_code."',".$ttl.",'".$protocol_name."','".$protocol_flags_code."','".$protocol_checksum_status."',".$protocol_ports_from.",".$protocol_ports_dest.",".$protocol_type.",".$protocol_code.",'".$headerChecksum."','".$ip_from."','".$ip_dest."')";
-      $query = $pdo->prepare($sql);
-      $query->execute();
-    }
-
-
-  }
-  else {
+  // QUAND PROTOCOLE ICMP
 
     /// VERIF QUE TOTO N'EXISTE PAS DEJA POUR PAS FAIRE DOUBLON BDD
     $sql = "SELECT * FROM trames WHERE datet = '$datet' AND protocol_name = '$protocol_name' AND identification = '$identification' AND protocol_flags_code = '$protocol_flags_code'";
@@ -61,12 +81,10 @@ foreach ($trames as $trame) {
     $trameExist = $query->fetch();
 
 
-    if(empty($trameExist)){
-      $sql = "INSERT INTO trames (datet,version,headerLength,service,identification,flags_code,ttl,protocol_name,protocol_flags_code,protocol_checksum_status,protocol_ports_from,protocol_ports_dest,headerChecksum,ip_from,ip_dest) VALUES (".$datet.",".$version.",".$headerLength.",'".$service."','".$identification."','".$flags_code."',".$ttl.",'".$protocol_name."','".$protocol_flags_code."','".$protocol_checksum_status."',".$protocol_ports_from.",".$protocol_ports_dest.",'".$headerChecksum."','".$ip_from."','".$ip_dest."')";
-      $query = $pdo->prepare($sql);
-      $query->execute();
-    }
-
+  if(empty($trameExist)){
+    $sql = "INSERT INTO trames (datet,version,headerLength,service,identification,flags_code,ttl,protocol_name,protocol_flags_code,protocol_checksum_status,protocol_ports_from,protocol_ports_dest,protocol_type,protocol_code,headerChecksum,ip_from,ip_dest) VALUES (".$datet.",".$version.",".$headerLength.",'".$service."','".$identification."','".$flags_code."',".$ttl.",'".$protocol_name."','".$protocol_flags_code."','".$protocol_checksum_status."',".$protocol_ports_from.",".$protocol_ports_dest.",".$protocol_type.",".$protocol_code.",'".$headerChecksum."','".$new_ip."','".$ip_dest."')";
+    $query = $pdo->prepare($sql);
+    $query->execute();
   }
 
 }
