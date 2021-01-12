616d53db695a1fa9454d5e34f8ed9508f9a0e0b9
VitorinoNzo <vitorinonzo@gmail.com>
2021-01-12 16:52:01 +0100

    faq + commentaire css+ reflexion prhases index

--- faq.php
+++ faq.php
@@ -14,7 +14,7 @@ include('inc/header.php');
         <div id="contenu" class="container-fluid mt-5 d-flex flex-row align-items-center">
             <h1 class="faq">Impossible de me connecter à mon compte, que faire ?</h1>
             <div class="container-fluid d-flex mt-5 flex-column pb-5">
-                <p class="text-center">Assurez vous de n'avoir fais aucune faute dans vos identifiants de connexion, sinon dirigez vous vers le bouton mot de passe oublié.</p>
+                <p class="text-center">Assurez vous de n'avoir fais aucune faute dans vos identifiants de connexion, sinon dirigez vous vers le bouton <a href="#">mot de passe oublié.</a></p>
             </div>
         </div>
         <div id="contenu" class="container-fluid mt-5 d-flex flex-row align-items-center">
@@ -24,9 +24,15 @@ include('inc/header.php');
             </div>
         </div>
         <div id="contenu" class="container-fluid mt-5 d-flex flex-row align-items-center">
-            <h1 class="faq">Impossible de me connecter à mon compte, que faire ?</h1>
+            <h1 class="faq">Quels protocoles utilisez-vous ?</h1>
             <div class="container-fluid d-flex mt-5 flex-column pb-5">
-                <p class="text-center">Assurez vous de n'avoir fais aucune faute dans vos identifiants de connexion, sinon dirigez vous vers le bouton mot de passe oublié.</p>
+                <p class="text-center">Afin de fournir un maximum d'information, nous analysons la trame TTL (Time to Live),qui est la durée de vie de votre trame, le TCP (Transmission Control Protocol), qui sert a contrôler la transmission entre l'utilisateur et le serveur. L'UDP (User Diagram Protocol), qui permet une bonne transmission de donnée entre vous et le serveur. Et pour finir, l'ICMP (Internet Control Message Protocol), qui sert à vous prevenir quand un site est inaccessible ou dangereux.</p>
+            </div>
+        </div>
+        <div id="contenu" class="container-fluid mt-5 d-flex flex-row align-items-center">
+            <h1 class="faq">Est-il possible de localiser le lieu de connexion de l'internaute ?</h1>
+            <div class="container-fluid d-flex mt-5 flex-column pb-5">
+                <p class="text-center">Il est possible de connaître le pays et la région à partir desquels l'internaute se connecte. Pour des régions très étendues comme le Canada, la précision géographique atteint la ville.</p>
             </div>
         </div>
     </div>
